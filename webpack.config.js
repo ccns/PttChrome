915cb6700bb77f54fdca04384061afa46036f01d
Hung-Te Lin <hungte@chromium.org>
2026-09-02 21:51:28 +0800

    fix(webpack): add querystring fallback for webpack 5 compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,9 @@ module.exports = {
     ]
   },
   resolve: {
+    fallback: {
+      querystring: false
+    },
     plugins: [new AliasPlugin('described-resolve', [{
       name: 'Icon',
       alias: [
